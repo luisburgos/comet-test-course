4a0d6d9612edc07fd9a6ab6fc380827c776a6c49
LuisBurgos <luis.burgos.1995@gmail.com>
2017-11-25 10:54:41 -0600

    Added factorial exercise.

---
+++ factorial/src/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+    public static long factorial(long n) {
+        // BEGIN SOLUTION
+        long result = 1;
+        for (long i = 2; i <= n; ++i) {
+            result *= i;
+        }
+        return result;
+        // END SOLUTION
+        // STUB: return 0; // Please write your code here
+    }
+}
