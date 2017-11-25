f2770aa3fb1e99e3dfede093890480c4d4e6379a
LuisBurgos <luis.burgos.1995@gmail.com>
2017-11-25 13:47:40 -0600

    Added seconds of the year exercise.

---
+++ secondsOfTheYear/src/SecondsOfTheYear.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luisburgos
+ */
+public class SecondsOfTheYear {
+
+    public static void main(String[] args) {   
+        // In below an incomplete version of the program. Please complete it!
+
+        int daysInYear = 365;
+        int hoursInDay = 24;
+        int minutesInHour = 60;
+        int secondsInMinute = 60;
+
+        int secondsInYear = daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+
+        System.out.println("There are " + secondsInYear + " seconds in a year");        
+    }
+
+}
